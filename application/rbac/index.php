0deeecc37ab1e0ac223b1fcc57fa01d9c9eea7b7
lidi <lidi0903@163.com>
2017-07-06 17:07:59 +0800

    ...

--- application/rbac/index.php
+++ application/rbac/index.php
@@ -1,6 +1,10 @@
 <?php
-require '../../system/db/mysql.class.php';
-$sql = "select * from wp_users";
-$query = DB_mysql::row_array($sql);
-echo '<pre>';
-print_r($query);
+// require '../../system/db/mysql.class.php';
+require '../../system/lib/page.class.php';
+$page = new Page(30, 1, 2, '/service/set');
+echo $page->showPage();
+
+// $sql = "select * from wp_users";
+// $query = DB_mysql::row_array($sql);
+// echo '<pre>';
+// print_r($query);
--- system/lib/page.class.php
+++ system/lib/page.class.php
@@ -30,39 +30,54 @@ class Page{
 	// 生成分页
 	public function showPage(){
 		$func = 'createPage' . $this->pageStyle;
-		if(function_exists($func)){
-			return $this->$func();
-		}else{
-			return 'The page style not find';
-		}
+		return $this->$func();
 	}
 
 	/**
-	 * 共30条记录，当前第1/5页 [首页] [上页] [1] [2] [3] .. [7] [8] [9] [下页] [尾页]
+	 * 共30条记录，当前第1/5页 [首页] [上页] [1] .. [4] [5] [6] [7] [8] .. [11] [下页] [尾页]
 	 */
 	private function createPage1(){
 		$pagestr = '';
 		$pagestr .= '共'.$this->count.'条记录，当前是第'.$this->currentPage.'/'.$this->countPage.'页';
 		if($this->currentPage>1){
-			$pagestr .= ' [<a href="'.$this->pageUrl.'?page=1'.'">首页</a>] ';
-			$pagestr .= ' [<a href="'.$this->pageUrl.'?page='.$this->currentPage-1.'">上页</a>] ';
+			$pagestr .= ' [<a href="'.($this->pageUrl.'?page=1').'">首页</a>] ';
+			$pagestr .= ' [<a href="'.($this->pageUrl.'?page='.($this->currentPage-1)).'">上页</a>] ';
 		}else{
 			$pagestr .= ' [<a href="javascript:void(0);">首页</a>] ';
 			$pagestr .= ' [<a href="javascript:void(0);">上页</a>] ';
 		}
 
-		if($this->countPage>6){
-
+		if($this->countPage<7){
+			for ($i=1; $i <= $this->countPage; $i++) {
+				if($i==$this->currentPage){
+					$pagestr .= ' [<a href="javascript:void(0);">'.$i.'</a>] ';
+				}else{
+					$pagestr .= ' [<a href="'.($this->pageUrl.'?page='.$i).'">'.$i.'</a>] ';
+				}
+			}
 		}else{
+			// 当前页>=4的时候，添加第一个页码元素
+			if($this->currentPage!=1 && $this->currentPage>=4 && $this->countPage!=4){
+				$pagestr .= ' [<a href="'.($this->pageUrl.'?page=1').'">1</a>] ';
+			}
+			// 页码>4，并且<=总页数，总页码>5，添加...
+			if($this->currentPage-2>2 && $this->currentPage<=$this->countPage && $this->currentPage>5){
+				$pagestr .= '...';
+			}
+			// 当前页码的前两页
+			$start_page = $this->currentPage-2;
+			// 当前页码的后两页
+			$end_page = $this->currentPage+2;
 
 		}
 
 		if($this->currentPage<$this->countPage){
-			$pagestr .= ' [<a href="'.$this->pageUrl.'?page='.$this->currentPage+1.'">下页</a>] ';
-			$pagestr .= ' [<a href="'.$this->pageUrl.'?page='.$this->countPage.'">尾页</a>] ';
+			$pagestr .= ' [<a href="'.($this->pageUrl.'?page='.($this->currentPage+1)).'">下页</a>] ';
+			$pagestr .= ' [<a href="'.($this->pageUrl.'?page='.$this->countPage).'">尾页</a>] ';
 		}else{
 			$pagestr .= ' [<a href="javascript:void(0);">下页</a>] ';
 			$pagestr .= ' [<a href="javascript:void(0);">尾页</a>] ';
 		}
+		return $pagestr;
 	}
 }
