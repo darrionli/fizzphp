dd11532465c5e407d58296c164b9b7bc4d3ade4f
lidi <lidi0903@163.com>
2018-05-26 22:56:08 +0800

    reg

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Routing\Route;
 
 class UserController extends Controller
 {
@@ -10,5 +12,29 @@ class UserController extends Controller
     public function create()
     {
         return view('users.create');
+    }
+
+    // 页面展示
+    public function show(User $user)
+    {
+        return view('users.show', compact('user'));
+    }
+
+    // 注册
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name'=>'required|max:50',
+            'email'=>'required|email|unique:users|max:255',
+            'password'=>'required|confirmed|min:6'
+        ]);
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+
+        session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
+        return redirect()->route('users.show', [$user]);
     }
 }
