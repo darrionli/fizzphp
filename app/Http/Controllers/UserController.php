a3bbd518da411e0583edd1cee6db6b899ea24590
lidi <lidi0903@163.com>
2018-05-27 11:12:23 +0800

    email send

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,13 +5,14 @@ namespace App\Http\Controllers;
 use App\User;
 use Illuminate\Http\Request;
 use Auth;
+use Mail;
 
 class UserController extends Controller
 {
     public function __construct()
     {
         $this->middleware('auth', [
-           'except' => ['show', 'create', 'store','index']
+           'except' => ['show', 'create', 'store','index', 'confirmEmail']
         ]);
 
         $this->middleware('guest', [
@@ -45,8 +46,8 @@ class UserController extends Controller
             'password' => bcrypt($request->password),
         ]);
 
-        Auth::login($user);
-        session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
+        $this->sendEmailConfirmTo($user);
+        session()->flash('success', '验证邮件已发送到你的注册邮箱上，请注意查收。');
         return redirect()->route('users.show', [$user]);
     }
 
@@ -90,5 +91,31 @@ class UserController extends Controller
         $user->delete();
         session()->flash('success', '成功删除用户！');
         return back();
+    }
+
+    protected function sendEmailConfirmTo($user)
+    {
+        $view  = 'email.confirm';
+        $data = compact('user');
+        $from = 'lidi0903@163.com';
+        $name = 'lidi';
+        $to = $user->email;
+        $subject = '感谢注册Sample应用，请确认您的邮箱。';
+        Mail::send($view, $data, function($message) use ($from, $name, $to, $subject){
+            $message->from($from, $name)->to($to)->subject($subject);
+        });
+    }
+
+    public function confirmEmail($token)
+    {
+        $user = User::where('activation_token', $token)->firstOrFail();
+
+        $user->activated = true;
+        $user->activation_token = null;
+        $user->save();
+
+        Auth::login($user);
+        session()->flash('success', '恭喜你，激活成功！');
+        return redirect()->route('users.show', [$user]);
     }
 }
